094e9aecd4b22197239663e991f6d033f6050f7b
dennisobel <ogembodenis2016@gmail.com>
2023-05-03 16:44:10 +0300

    Marker color changed, using lwarence data

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,18 +1,15 @@
 // import MapMarker from "../assets/map.png"
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Map, { Marker, Popup, NavigationControl } from 'react-map-gl';
+import data from "./data"
 
-const MapView = () => {
+const MapView = ({ markers }) => {
     const MapMarker = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="%23ffffff"%3E%3Cpath d="M12 2C8.13 2 5 5.13 5 9c0 6 7 13 7 13s7-7 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" /%3E%3C/svg%3E'
 
-    const markers = [
-        { latitude: -1.2865303324119353, longitude: 36.82099936425639, label: 'Nairobi City Centre' },
-        { latitude: -1.2784, longitude: 36.8169, label: 'Kenyatta International Convention Centre' },
-        { latitude: -1.2854, longitude: 36.8211, label: 'University of Nairobi' },
-        { latitude: -1.2866, longitude: 36.8266, label: 'Nairobi National Museum' },
-    ];
+    const selectedMarkerRef = useRef();
 
-    const [selectedMarker, setSelectedMarker] = useState(null);
+    const [selectedMarker, setSelectedMarker] = useState();
+    const [popupOpen, setPopupOpen] = useState(false);
     const [viewport, setViewport] = useState({
         latitude: -1.2921,
         longitude: 36.8268,
@@ -23,8 +20,26 @@ const MapView = () => {
         setViewport(newViewport);
     };
 
-    // mapStyle="mapbox://styles/mapbox/streets-v9"
-    //         mapboxAccessToken='pk.eyJ1IjoiZGVubmlzb2JlbCIsImEiOiJjbGg1dmxqYTMwMDBnM2VwaTk2MXNvemh3In0.PCXH5DjguDHHgmaHviCFow'
+    const handleSelected = (marker) => {
+        console.log('marker clicked:', marker);
+        setSelectedMarker((prevMarker) => prevMarker === marker ? null : marker);
+        setPopupOpen(true);
+    };
+
+    const handleClosePopup = () => {
+        setSelectedMarker(null);
+        setPopupOpen(false);
+    };
+
+    useEffect(() => {
+        selectedMarkerRef.current = selectedMarker;
+        console.log('selectedMarker:', selectedMarker);
+        console.log('selectedMarkerRef:', selectedMarkerRef.current);
+        return () => {
+            selectedMarkerRef.current = null;
+        };
+    }, [selectedMarker]);
+
 
     return (
         <Map
@@ -34,30 +49,27 @@ const MapView = () => {
             mapStyle="mapbox://styles/wesley254/clezjwl8d002001md18wexpan"
             mapboxAccessToken='pk.eyJ1Ijoid2VzbGV5MjU0IiwiYSI6ImNsMzY2dnA0MDAzem0zZG8wZTFzc3B3eG8ifQ.EVg7Sg3_wpa_QO6EJjj9-g'
         >
-
             {markers.map((marker, index) => (
-                <Marker key={index} latitude={marker.latitude} longitude={marker.longitude} anchor="bottom">
-                    <img onClick={() => setSelectedMarker(marker)} style={{ height: "30px", width: "30px", cursor: "pointer" }} src={MapMarker} alt="mapmarker" />
+                <Marker key={index} latitude={marker.properties.latitude} longitude={marker.properties.longitude} anchor="bottom">
+                    <img onClick={() => handleSelected(marker)} style={{ height: "30px", width: "30px", cursor: "pointer", backgroundColor:"teal" }} src={MapMarker} alt="mapmarker" />
                 </Marker>
             ))}
-            {selectedMarker && (
+
+            {selectedMarkerRef.current && (
                 <Popup
-                    latitude={selectedMarker.latitude}
-                    longitude={selectedMarker.longitude}
-                    onClose={() => setSelectedMarker(null)}
-                    anchor="bottom"
+                    latitude={selectedMarkerRef.current.latitude + 0.01}
+                    longitude={selectedMarkerRef.current.longitude + 0.01}
+                    anchor="top-left"
+                    onClose={handleClosePopup}
                 >
                     <div>
-                        <h3>{selectedMarker.label}</h3>
-                        {/* <p>{selectedMarker.description}</p> */}
+                        <h3>{selectedMarkerRef.current.label}</h3>
                     </div>
                 </Popup>
             )}
             <NavigationControl showCompass showZoom position='top-left' />
-
         </Map>
     );
 };
 
 export default MapView;
-
--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -5,48 +5,24 @@ import Header from "components/Header";
 import { ResponsiveChoropleth } from "@nivo/geo";
 import { geoData } from "state/geoData";
 import { useSelector } from "react-redux";
+import data from "./data"
 
 import MapView from "components/Map";
 
 const Geography = () => {
   const searchQuery = useSelector(state => state.global.searchQuery)
   const theme = useTheme();
-  const { data } = useGetGeographyQuery();
+  
 
   console.log(searchQuery)
 
-  const [markers,setMarkers] = useState([
-    { latitude: -1.2747, longitude: 36.8219, label: 'Nairobi City Centre' },
-    { latitude: -1.2784, longitude: 36.8169, label: 'Kenyatta International Convention Centre' },
-    { latitude: -1.2854, longitude: 36.8211, label: 'University of Nairobi' },
-    { latitude: -1.2866, longitude: 36.8266, label: 'Nairobi National Museum' },
-    { latitude: -1.3015, longitude: 36.8282, label: 'Giraffe Centre' },
-    { latitude: -1.3157, longitude: 36.7947, label: 'Nairobi National Park' },
-    { latitude: -1.2849, longitude: 36.8282, label: 'David Sheldrick Wildlife Trust' },
-    { latitude: -1.2884, longitude: 36.8214, label: 'Uhuru Park' },
-    { latitude: -1.2986, longitude: 36.7749, label: 'Jomo Kenyatta International Airport' },
-    { latitude: -1.2761, longitude: 36.8008, label: 'Nairobi Railway Station' },
-    { latitude: -1.2748, longitude: 36.7922, label: 'Nairobi Bus Station' },
-    { latitude: -1.2835, longitude: 36.8165, label: 'Nairobi Hospital' },
-    { latitude: -1.2945, longitude: 36.8072, label: 'Yaya Centre' },
-    { latitude: -1.2899, longitude: 36.8203, label: 'Central Park' },
-    { latitude: -1.2914, longitude: 36.8206, label: 'August 7th Memorial Park' },
-    { latitude: -1.2864, longitude: 36.8267, label: 'Snake Park' },
-    { latitude: -1.2792, longitude: 36.8454, label: 'City Hall' },
-    { latitude: -1.2921, longitude: 36.8268, label: 'Nairobi Gallery' },
-    { latitude: -1.3066, longitude: 36.7572, label: 'Nairobi Safari Walk' },
-    { latitude: -1.2927, longitude: 36.8189, label: 'Kenya National Theatre' },
-    { latitude: -1.2864, longitude: 36.8129, label: 'State House Nairobi' },
-    { latitude: -1.2922, longitude: 36.8132, label: 'Nairobi Arboretum' },
-    { latitude: -1.3027, longitude: 36.7697, label: 'Wilson Airport' },
-    { latitude: -1.2753, longitude: 36.8133, label: 'Kenya National Archives' }
-  ]);
+  const markers = data.features;
 
   const [filteredMarkers,setFilteredMarkers] = useState([...markers])
 
   useEffect(()=>{
     let filtered = markers.filter(marker => {
-      return marker.label.includes(searchQuery)
+      return marker.properties.ward.includes(searchQuery)
     })
     setFilteredMarkers(filtered)
   },[searchQuery])
